1bc1c0dec91a9d50bc1d2587055378c17e8a173c
AkankshaArun <akanksha.arun26@gmail.com>
2020-05-11 19:14:37 +0530

    Added Redux and Alert

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,20 +2,26 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  //get token from header
+  // Get token from header
   const token = req.header('x-auth-token');
 
-  //Check if no token
-  if (!token)
-    return res.status(401).json({ msg: 'No Token, Authorization Denied' });
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-  //Verify Token if present by decoding
+  // Verify token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-    //Since decoding is successful, make the user decoded
-    req.user = decoded.user;
-    next();
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
   } catch (err) {
-    res.status(401).json({ msg: 'Invalid Token' });
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
   }
 };
